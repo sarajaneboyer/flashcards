fa20da99a8a30cece633c3c6a4dff8ffc833ab95
Sara Boyer <sara.jane.boyer@gmail.com>
2020-03-05 21:36:08 -0700

    comments added

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 
 q1 = document.getElementById('q1');
 a1 = document.getElementById('a1');
+// show-button1 = document.getElementById('show-button1');
+// showbutton2 = document.getElementById('show-button2');
+// showbutton3 = document.getElementById('show-button3');
 
 showButton1 = document.getElementById('show-button1');
 showButton1.addEventListener('click', () => {
@@ -10,7 +13,7 @@ showButton1.addEventListener('click', () => {
 hideButton1 = document.getElementById('hide-button1');
 hideButton1.addEventListener('click', () => {
   a1.classList.add("hide")
-})
+});
 
 showButton2 = document.getElementById('show-button2');
 showButton2.addEventListener('click', () => {
@@ -19,8 +22,8 @@ showButton2.addEventListener('click', () => {
 
 hideButton2 = document.getElementById('hide-button2');
 hideButton2.addEventListener('click', () => {
-  a1.classList.add("hide")
-})
+  a2.classList.add("hide")
+});
 
 showButton3 = document.getElementById('show-button3');
 showButton3.addEventListener('click', () => {
@@ -30,7 +33,31 @@ showButton3.addEventListener('click', () => {
 hideButton1 = document.getElementById('hide-button3');
 hideButton1.addEventListener('click', () => {
   a3.classList.add("hide")
+});
+
+//these are not actually deleting them but are hiding them. I can't figure out how to "delete" when they are hard coded into my html and not in an array like deckArray from user input...
+deleteButton1 = document.getElementById('delete-button1');
+deleteButton1.addEventListener('click', () => {
+  q1.classList.add("hide");
+  a1.classList.add("hide");
+  // should also hide the button that "shows answer 1"
+  //this code is breaking it and making my "delete" button stop working
+  //its the definition of show-button1 up at the top of the script that is breaking it and i'm not sure why
+  // show-button1.add("hide");
 })
+
+deleteButton2 = document.getElementById('delete-button2');
+deleteButton2.addEventListener('click', () => {
+  q2.classList.add("hide");
+  a2.classList.add("hide");
+})
+
+deleteButton3 = document.getElementById('delete-button3');
+deleteButton3.addEventListener('click', () => {
+  q3.classList.add("hide");
+  a3.classList.add("hide");
+})
+
 
 
 
