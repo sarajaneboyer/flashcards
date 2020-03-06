7fe94958d7129500cb26a858802aa2482cded565
Sara Boyer <sara.jane.boyer@gmail.com>
2020-03-05 21:20:34 -0700

    hide and show buttons updated to flex-box and deleted old css

--- scripts.js
+++ scripts.js
@@ -2,12 +2,50 @@
 q1 = document.getElementById('q1');
 a1 = document.getElementById('a1');
 
-
-flipButton1 = document.getElementById('flip-button1');
-flipButton1.addEventListener("click", () => {
+showButton1 = document.getElementById('show-button1');
+showButton1.addEventListener('click', () => {
   a1.classList.remove("hide");
-  q1.classList.add("hide"); 
+});
+
+hideButton1 = document.getElementById('hide-button1');
+hideButton1.addEventListener('click', () => {
+  a1.classList.add("hide")
 })
+
+showButton2 = document.getElementById('show-button2');
+showButton2.addEventListener('click', () => {
+  a2.classList.remove("hide");
+});
+
+hideButton2 = document.getElementById('hide-button2');
+hideButton2.addEventListener('click', () => {
+  a1.classList.add("hide")
+})
+
+showButton3 = document.getElementById('show-button3');
+showButton3.addEventListener('click', () => {
+  a3.classList.remove("hide");
+});
+
+hideButton1 = document.getElementById('hide-button3');
+hideButton1.addEventListener('click', () => {
+  a3.classList.add("hide")
+})
+
+
+
+
+// var z = a1.className
+// if (a1.className = 'hide') {
+//   a1.classList.remove('hide')
+// }
+// else { a1.classList.add('hide')}
+
+// flipButton1 = document.getElementById('flip-button1');
+// flipButton1.addEventListener("click", () => {
+//   a1.classList.remove("hide");
+//   q1.classList.add("hide"); 
+// })
 
 flipButton2 = document.getElementById('flip-button2');
 flipButton2.addEventListener("click", () => {
